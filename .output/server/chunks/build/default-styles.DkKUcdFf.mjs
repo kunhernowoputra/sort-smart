73fbe064de2eb5e49762e4cb1a49eab93c367f20
Kun Hernowo Putra <kunhernowoputra@linux.local>
2026-03-12 12:18:03 +0700

    chore: add .gitignore and cleanup repository

--- .output/server/chunks/build/default-styles.DkKUcdFf.mjs
+++
@@ -1,8 +0,0 @@
-const default_vue_vue_type_style_index_0_scoped_11f21f3f_lang = ".custom-scrollbar[data-v-11f21f3f]::-webkit-scrollbar{width:6px}.custom-scrollbar[data-v-11f21f3f]::-webkit-scrollbar-track{background:transparent}.custom-scrollbar[data-v-11f21f3f]::-webkit-scrollbar-thumb{background:#ffffff0d;border-radius:10px}.custom-scrollbar[data-v-11f21f3f]::-webkit-scrollbar-thumb:hover{background:#ffffff1a}";
-
-const defaultStyles_DkKUcdFf = [
-  default_vue_vue_type_style_index_0_scoped_11f21f3f_lang
-];
-
-export { defaultStyles_DkKUcdFf as default };
-//# sourceMappingURL=default-styles.DkKUcdFf.mjs.map
